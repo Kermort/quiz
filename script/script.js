6bf7767f8445d79783176da1d5693034be18bf51
Kermort <64635345+Kermort@users.noreply.github.com>
2020-05-31 20:37:55 +1000

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,42 @@
+var pageLoaded = false;
+
+window.onload = function() {
+   PageLoaded = true;
+   console.log("страница загружена");
+   
+}
+
+
+// function init() {
+//     request.open("GET", requestURL);
+//     request.responseType = "json";
+//     request.send;
+//     request.onload = function() {
+//         console.log("ответ пришел")
+//         questionsList = request.response;
+//     }
+
+// }
+
+function startGame() {
+
+}
+
+document.getElementById("button-start").onclick = function() {
+    var requestQuestionFile = "../files/questions.json";
+    // var requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    var request = new XMLHttpRequest();
+    var questionsList;
+    request.open("GET", requestQuestionFile, true);
+    request.responseType = "json";
+    request.send();
+    request.onload = function() {
+        console.log("ответ пришел")
+        questionsList = request.response;
+        console.log(questionsList);
+        console.log(request);
+        console.log(requestQuestionFile);
+    }
+console.log("start");
+console.log(request);
+}
